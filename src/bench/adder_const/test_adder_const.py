1d99152296b5912dd6f1c6555a05dcbbbd3886d7
UF523TCH <timothee.charrier@elsys-design.com>
2025-01-15 17:09:42 +0100

    feat: add testbench and utility library for AdderConst module in ASCON

---
+++ src/bench/adder_const/test_adder_const.py
@@ -0,0 +1,207 @@
+"""
+Testbench for the BatchNorm2d Layer.
+
+This module tests the BatchNorm2d layer function module by comparing the
+output of the Python implementation with the VHDL implementation.
+
+@author: Timothée Charrier
+"""
+
+import os
+import random
+import sys
+from pathlib import Path
+
+import cocotb
+from cocotb.runner import get_runner
+from cocotb.triggers import Timer
+
+# Add the directory containing the utils.py file to the Python path
+sys.path.insert(0, str((Path(__file__).parent.parent).resolve()))
+
+from ascon_utils import (
+    INPUT_STATE,
+    AdderConstModel,
+)
+from cocotb_utils import (
+    ERRORS,
+    init_hierarchy,
+)
+
+# Define the IOs and their default values at reset
+INIT_INPUTS = {
+    "i_state": init_hierarchy(dims=(5,), bitwidth=64, use_random=False),
+    "i_round": 0,
+}
+
+
+def assert_output(
+    dut: cocotb.handle.HierarchyObject,
+    input_state: list[int],
+    expected_output: list[int],
+) -> None:
+    """
+    Assert the output of the DUT and log the input and output values.
+
+    Parameters
+    ----------
+    dut : cocotb.handle.HierarchyObject
+        The device under test (DUT).
+    input_state : list[int]
+        The input state values.
+    expected_output : list[int]
+        The expected output values.
+    dut_output : list[cocotb.binary.BinaryValue]
+        The DUT output values.
+
+    """
+    input_str = " ".join([f"{x & 0xFFFFFFFFFFFFFFFF:016X}" for x in input_state])
+    output_str = " ".join([f"{x & 0xFFFFFFFFFFFFFFFF:016X}" for x in expected_output])
+    output_dut_str = " ".join(
+        [f"{x.value.integer & 0xFFFFFFFFFFFFFFFF:016X}" for x in dut.o_state]
+    )
+
+    dut._log.info(f"Input:      {input_str}")
+    dut._log.info(f"Expected:   {output_str}")
+    dut._log.info(f"DUT Output: {output_dut_str}")
+    dut._log.info("")
+
+    assert output_str == output_dut_str, ERRORS["FAILED_COMPUTATION"]
+
+
+@cocotb.test()
+async def reset_dut_test(dut: cocotb.handle.HierarchyObject) -> None:
+    """
+    Test the DUT's behavior during reset.
+
+    Verifies that the output is correctly reset and remains stable.
+
+    Parameters
+    ----------
+    dut : object
+        The device under test (DUT).
+
+    """
+    try:
+        # Define the model
+        adder_model = AdderConstModel(INIT_INPUTS["i_state"])
+
+        # Initialize the DUT
+        for key, value in INIT_INPUTS.items():
+            getattr(dut, key).value = value
+
+        # Wait for few ns (combinatorial logic only in the DUT)
+        await Timer(10, units="ns")
+
+        # Check the output
+        adder_output = adder_model.compute(i_round=INIT_INPUTS["i_round"])
+
+        # Log Input and output values as hex
+        assert_output(dut, INIT_INPUTS["i_state"], adder_output)
+
+    except Exception as e:
+        raise RuntimeError(ERRORS["FAILED_RESET"].format(e=e)) from e
+
+
+@cocotb.test()
+async def adder_const_test(dut: cocotb.handle.HierarchyObject) -> None:
+    """Test the DUT's behavior during normal computation."""
+    try:
+        # Define the model
+        adder_model = AdderConstModel(INIT_INPUTS["i_state"])
+
+        await reset_dut_test(dut)
+
+        # Set specific inputs defined by INPUT_STATE = [IV, P1, P2, P3, P4]
+        dut.i_state.value = INPUT_STATE
+
+        # Wait for few ns
+        await Timer(10, units="ns")
+
+        # Check the output
+        adder_model.update_state(INPUT_STATE)
+        adder_output = adder_model.compute(i_round=0)
+
+        # Assert and log the output
+        assert_output(dut, INPUT_STATE, adder_output)
+
+        dut._log.info("Starting random tests...")
+
+        # Try with random inputs
+        for _ in range(10):
+            # Generate random inputs
+            random_state = init_hierarchy(dims=(5,), bitwidth=64, use_random=True)
+            random_round = random.randint(0, 10)
+
+            # Set the inputs
+            dut.i_state.value = random_state
+            dut.i_round.value = random_round
+
+            # Wait for few ns
+            await Timer(10, units="ns")
+
+            # Check the output
+            adder_model.update_state(random_state)
+            adder_output = adder_model.compute(i_round=random_round)
+
+            # Assert and log the output
+            assert_output(dut, random_state, adder_output)
+
+    except Exception as e:
+        raise RuntimeError(ERRORS["FAILED_SIMULATION"].format(e=e)) from e
+
+
+def test_adder_const() -> None:
+    """Function Invoked by the test runner to execute the tests."""
+    # Define the simulator to use
+    default_simulator = "verilator"
+
+    # Define LIB_RTL
+    library = "LIB_RTL"
+
+    # Define rtl_path
+    rtl_path = (Path(__file__).parent.parent.parent / "rtl/").resolve()
+
+    # Define the sources
+    sources = [
+        f"{rtl_path}/ascon_pkg.v",
+        f"{rtl_path}/adder_const/adder_const.v",
+    ]
+
+    # Top-level HDL entity
+    entity = "adder_const"
+
+    try:
+        # Get simulator name from environment
+        simulator = os.environ.get("SIM", default_simulator)
+
+        # Initialize the test runner
+        runner = get_runner(simulator_name=simulator)
+
+        # Build HDL sources
+        runner.build(
+            build_dir="sim_build",
+            clean=True,
+            hdl_library=library,
+            hdl_toplevel=entity,
+            sources=sources,
+            verbose=True,
+            waves=True,
+        )
+
+        # Run tests
+        runner.test(
+            build_dir="sim_build",
+            hdl_toplevel=entity,
+            hdl_toplevel_library=library,
+            test_module=f"test_{entity}",
+            verbose=True,
+            waves=True,
+        )
+
+    except Exception as e:
+        raise RuntimeError(ERRORS["FAILED_COMPILATION"].format(e=e)) from e
+
+
+if __name__ == "__main__":
+    test_adder_const()
---
+++ src/bench/ascon_utils.py
@@ -0,0 +1,224 @@
+"""
+Library for the Ascon Testbench.
+
+It contains the Python model used to verify all the Ascon modules.
+
+@author: Timothée Charrier
+"""
+
+from __future__ import annotations
+
+# ======================================================================================
+# Constants used in the Ascon modules Testbenches.
+# ======================================================================================
+IV = 0x80400C0600000000
+KEY = 0x000102030405060708090A0B0C0D0E0F
+NONCE = 0x000102030405060708090A0B0C0D0E0F
+ASSOCIATED_DATA_32 = 0x32303232
+ASSOCIATED_DATA_64 = (ASSOCIATED_DATA_32 << 32) | 0x80000000
+P1 = 0x446576656C6F7070
+P2 = 0x657A204153434F4E
+P3 = 0x20656E206C616E67
+P4 = 0x6167652056484480
+INPUT_STATE = [
+    IV,
+    (KEY >> 64) & 0xFFFFFFFFFFFFFFFF,  # Upper 64 bits of KEY
+    KEY & 0xFFFFFFFFFFFFFFFF,  # Lower 64 bits of KEY
+    (NONCE >> 64) & 0xFFFFFFFFFFFFFFFF,  # Upper 64 bits of NONCE
+    NONCE & 0xFFFFFFFFFFFFFFFF,  # Lower 64 bits of NONCE
+]
+S_TABLE = [
+    0x04,
+    0x0B,
+    0x1F,
+    0x14,
+    0x1A,
+    0x15,
+    0x09,
+    0x02,
+    0x1B,
+    0x05,
+    0x08,
+    0x12,
+    0x1D,
+    0x03,
+    0x06,
+    0x1C,
+    0x1E,
+    0x13,
+    0x07,
+    0x0E,
+    0x00,
+    0x0D,
+    0x11,
+    0x18,
+    0x10,
+    0x0C,
+    0x01,
+    0x19,
+    0x16,
+    0x0A,
+    0x0F,
+    0x17,
+]
+# ======================================================================================
+
+
+class AdderConstModel:
+    """
+    Model for the AdderConst module.
+
+    This class defines the model used to verify the AdderConst module.
+    """
+
+    def __init__(
+        self,
+        i_state: list[int],
+        *,
+        constant_array_t: list[int] | None = None,
+    ) -> None:
+        """
+        Initialize the model.
+
+        Parameters
+        ----------
+        i_state : list[int]
+            Initial state of the inputs.
+        constant_array_t : list[int], optional
+            Array of constants used in the computation.
+            If not provided, a default array is used.
+
+        """
+        self.i_state = i_state
+        self.constant_array_t = constant_array_t or [
+            0xF0,
+            0xE1,
+            0xD2,
+            0xC3,
+            0xB4,
+            0xA5,
+            0x96,
+            0x87,
+            0x78,
+            0x69,
+            0x5A,
+            0x4B,
+        ]
+        self.o_state = [0] * len(i_state)
+
+    def compute(self, i_round: int) -> list[int]:
+        """
+        Compute the output state based on the current input state and round.
+
+        Parameters
+        ----------
+        i_round : int
+            The current round of computation.
+
+        Returns
+        -------
+        list[int]
+            The computed output state.
+
+        """
+        self.o_state[0] = self.i_state[0]
+        self.o_state[1] = self.i_state[1]
+        self.o_state[2] = (self.i_state[2] & 0xFFFFFFFFFFFFFF00) | (
+            self.i_state[2] ^ (self.constant_array_t[i_round] & 0xFF)
+        )
+        self.o_state[3] = self.i_state[3]
+        self.o_state[4] = self.i_state[4]
+        return self.o_state
+
+    def update_state(self, new_state: list[int]) -> None:
+        """
+        Update the input state of the model.
+
+        Parameters
+        ----------
+        new_state : list[int]
+            The new state to be set.
+
+        """
+        self.i_state = new_state
+
+    def get_output(self) -> list[int]:
+        """
+        Get the current output state.
+
+        Returns
+        -------
+        list[int]
+            The current output state.
+
+        """
+        return self.o_state
+
+
+class SboxModel:
+    """
+    Model for the SBOX module.
+
+    This class defines the model used to verify the SBOX module.
+    """
+
+    def __init__(
+        self,
+        *,
+        s_table: list[int] | None = None,
+    ) -> None:
+        """
+        Initialize the model.
+
+        Parameters
+        ----------
+        s_table : list[int]
+            The S-Box lookup table.
+
+        """
+        self.s_table = s_table or S_TABLE
+
+    def compute(self, i_data: int) -> int:
+        """
+        Compute the output data based on the input data.
+
+        Parameters
+        ----------
+        i_data : int
+            The input data.
+
+        Returns
+        -------
+        int
+            The computed output data.
+
+        """
+        return self.s_table[i_data]
+
+
+class SubLayerModel:
+    """
+    Model for the Substitution Layer module.
+
+    This class defines the model used to verify the Substitution Layer module.
+    """
+
+    def __init__(
+        self,
+        num_sboxes: int = 64,
+        *,
+        s_table: list[int] = S_TABLE,
+    ) -> None:
+        """
+        Initialize the model.
+
+        Parameters
+        ----------
+        num_sboxes : int, optional
+            The number of S-Boxes. Default is 64.
+        s_table : list[int], optional
+            The S-Box lookup table. If not provided, a default table is used.
+
+        """
+        self.num_sboxes = num_sboxes
+        self.s_table = s_table or S_TABLE
