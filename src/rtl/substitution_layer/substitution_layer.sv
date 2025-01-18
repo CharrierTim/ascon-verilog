bd31c3e196a23dfd93bd8dcbb1d907addb177228
CharrierTim <timothee.charrier@bbox.fr>
2025-01-18 23:59:46 +0100

    refactor: simplify generation of S-box instances in substitution_layer

--- src/rtl/substitution_layer/substitution_layer.sv
+++ src/rtl/substitution_layer/substitution_layer.sv
@@ -12,14 +12,11 @@ module substitution_layer
     output t_state_array o_state   //! Output State Array
 );
 
-  genvar i;
-  generate
-    for (i = 0; i < NUM_SBOXES; i = i + 1) begin : g_sbox
-      sbox sbox_i (
-          .i_data({i_state[0][i], i_state[1][i], i_state[2][i], i_state[3][i], i_state[4][i]}),
-          .o_data({o_state[0][i], o_state[1][i], o_state[2][i], o_state[3][i], o_state[4][i]})
-      );
-    end
-  endgenerate
+  for (genvar i = 0; i < NUM_SBOXES; i = i + 1) begin : g_sbox
+    sbox sbox_i (
+        .i_data({i_state[0][i], i_state[1][i], i_state[2][i], i_state[3][i], i_state[4][i]}),
+        .o_data({o_state[0][i], o_state[1][i], o_state[2][i], o_state[3][i], o_state[4][i]})
+    );
+  end
 
 endmodule
