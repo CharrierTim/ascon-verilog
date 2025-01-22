3d0ddcc1b3c184f95c0d1b22f19e077b38b723d9
UF523TCH <timothee.charrier@elsys-design.com>
2025-01-22 16:40:19 +0100

    refactor: enhance documentation and organization in ascon_pkg module

--- src/rtl/ascon_pkg.sv
+++ src/rtl/ascon_pkg.sv
@@ -1,3 +1,15 @@
+// filepath: ~/PROJET_ASCON/src/rtl/ascon_pkg.sv
+//------------------------------------------------------------------------------
+// Module Name      : ascon_pkg
+// Author           : Timothée Charrier
+// Date             : 2025-01-22
+// Description      : This package contains the type definitions and constants
+//                    used in the ASCON 128 cryptographic algorithm.
+//------------------------------------------------------------------------------
+// Revision History :
+//   - 2025-01-22
+//------------------------------------------------------------------------------
+
 `timescale 1ns / 1ps
 
 `ifndef ASCON_PKG_V
@@ -9,21 +21,37 @@ package ascon_pkg;
     // TYPE DEFINITIONS
     //
 
+    // State array used in the ASCON algorithm
     typedef logic [63:0] t_state_array[0:4];
+
+    // Array for round constants
     typedef logic [7:0] constant_array_t[0:11];
+
+    // Substitution table type
     typedef logic [7:0] substitution_table_t[0:31];
 
     //
     // CONSTANT DEFINITIONS
     //
 
-    const
-    constant_array_t
-    ROUND_CONSTANTS = '{8'hF0, 8'hE1, 8'hD2, 8'hC3, 8'hB4, 8'hA5, 8'h96, 8'h87, 8'h78, 8'h69, 8'h5A, 8'h4B};
+    // Round constants for ASCON
+    localparam constant_array_t ROUND_CONSTANTS = '{
+        8'hF0,
+        8'hE1,
+        8'hD2,
+        8'hC3,
+        8'hB4,
+        8'hA5,
+        8'h96,
+        8'h87,
+        8'h78,
+        8'h69,
+        8'h5A,
+        8'h4B
+    };
 
-    const
-    substitution_table_t
-    S_TABLE = '{
+    // Substitution table for ASCON
+    localparam substitution_table_t S_TABLE = '{
         8'h04,
         8'h0B,
         8'h1F,
