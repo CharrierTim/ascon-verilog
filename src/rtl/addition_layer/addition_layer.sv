583e215c8829473b516741d6b217d5e135d3c5d6
CharrierTim <timothee.charrier@bbox.fr>
2025-02-19 10:47:08 +0100

    refactor: rename lookup table for consistency and remove unused assignment in addition_layer

--- src/rtl/addition_layer/addition_layer.sv
+++ src/rtl/addition_layer/addition_layer.sv
@@ -8,13 +8,15 @@
 //------------------------------------------------------------------------------
 // Revision History :
 //   - 2025-01-22
+//   - 2025-02-19   : rename lookup table for consistency and remove non-used
+//                    assignment.
 //------------------------------------------------------------------------------
 
 `timescale 1ns / 1ps
 
 
 module addition_layer
-    import ascon_pkg::t_state_array, ascon_pkg::ROUND_CONSTANTS;
+    import ascon_pkg::t_state_array, ascon_pkg::LUT_ADDITION;
 (
     input  logic         [3:0] i_round,  //! Input round number, used to select round constant
     input  t_state_array       i_state,  //! Input State Array
@@ -25,10 +27,10 @@ module addition_layer
     // Output Assignment
     //
 
-    assign o_state[0]       = i_state[0];
-    assign o_state[1]       = i_state[1];
-    assign o_state[2][63:0] = i_state[2][63:0] ^ {56'h00000000000000, ROUND_CONSTANTS[i_round]};
-    assign o_state[3]       = i_state[3];
-    assign o_state[4]       = i_state[4];
+    assign o_state[0] = i_state[0];
+    assign o_state[1] = i_state[1];
+    assign o_state[2] = i_state[2] ^ {56'h00000000000000, LUT_ADDITION[i_round]};
+    assign o_state[3] = i_state[3];
+    assign o_state[4] = i_state[4];
 
 endmodule
