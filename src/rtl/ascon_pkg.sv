a0ef2a59f2abc7cd240235357c6a100a777214fe
CharrierTim <timothee.charrier@bbox.fr>
2025-06-05 19:58:07 +0200

    refactor: update header documentation in ascon_pkg.sv

--- src/rtl/ascon_pkg.sv
+++ src/rtl/ascon_pkg.sv
@@ -1,16 +1,41 @@
-// filepath: ~/ascon-verilog/src/rtl/ascon_pkg.sv
-//------------------------------------------------------------------------------
-// Module Name      : ascon_pkg
-// Author           : Timothée Charrier
-// Date             : 2025-01-22
-// Description      : This package contains the type definitions and constants
-//                    used in the ASCON 128 cryptographic algorithm.
-//------------------------------------------------------------------------------
-// Revision History :
-//   - 2025-01-22
-//   - 2025-02-19   : rename type definitions and constants for consistency
-//------------------------------------------------------------------------------
-
+/*
+ ***********************************************************************************************************************
+ *  MIT License
+ *
+ *  Copyright (c) 2025 Timothée Charrier
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ ***********************************************************************************************************************
+ * @file    ascon_pkg.sv
+ * @brief   ASCON package containing type definitions and constants
+ * @author  Timothée Charrier
+ * @date    2025-01-22
+ ***********************************************************************************************************************
+ * @version 1.1.0
+ * @date    2025-02-19
+ * @note    Rename type definitions and constants for consistency.
+ ***********************************************************************************************************************
+ * @version 1.0.0
+ * @date    2025-01-22
+ * @note    This package defines types and constants used in the ASCON algorithm.
+ ***********************************************************************************************************************
+ */
 `timescale 1ns / 1ps
 
 `ifndef ASCON_PKG_V
